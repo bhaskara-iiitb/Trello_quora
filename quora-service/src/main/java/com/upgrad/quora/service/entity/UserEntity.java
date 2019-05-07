b7b00951040ddf4a671b48b58b8306bd1c6cf91d
Bhaskara Rayavarapu <bhaskara.dsd4@iiitb.net>
2019-05-07 23:48:30 +0530

    First working version of working User SignUp endpoint

--- quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
@@ -6,12 +6,11 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "users", schema = "quora")
+@Table(name = "users"/*, schema = "quora"*/) //schema not required for public DB schema
 
 /*@NamedQueries(
         {
@@ -29,31 +28,34 @@ public class UserEntity implements Serializable {
     private Integer id;
 
     @Column(name = "UUID")
-    @Size(max = 60)
+    @NotNull
+    @Size(max = 200)
     private String uuid;
 
     @Column(name = "FIRSTNAME")
     @NotNull
-    @Size(max = 200)
+    @Size(max = 30)
     private String firstName;
 
 
     @Column(name = "LASTNAME")
     @NotNull
-    @Size(max = 200)
+    @Size(max = 30)
     private String lastName;
 
     @Column(name = "USERNAME")
     @NotNull
-    @Size(max = 200)
+    @Size(max = 30)
     private String username;
 
     @Column(name = "EMAIL")
     @NotNull
-    @Size(max = 200)
+    @Size(max = 50)
     private String email;
 
     @Column(name = "PASSWORD")
+    @NotNull
+    @Size(max = 225)
     private String password;
 
     @Column(name = "SALT")
@@ -62,21 +64,24 @@ public class UserEntity implements Serializable {
     private String salt;
 
     @Column(name = "COUNTRY")
-    @NotNull
-    @Size(max = 50)
+    @Size(max = 30)
     private String country;
 
     @Column(name = "ABOUTME")
-    @NotNull
     @Size(max = 50)
     private String aboutme;
 
-    @Column(name = "dob")
-    @NotNull
-    @Size(max = 50)
+    @Column(name = "DOB")
+    @Size(max = 30)
     private String dob;
 
+    @Column(name = "ROLE")
+    @Size(max = 30)
+    private String role;
 
+    @Column(name = "CONTACTNUMBER")
+    @Size(max = 30)
+    private String contactnumber;
 
     public Integer getId() {
         return id;
@@ -182,18 +187,6 @@ public class UserEntity implements Serializable {
         this.contactnumber = contactnumber;
     }
 
-    @Column(name = "ROLE")
-    private String role;
-
-
-    @Column(name = "CONTACTNUMBER")
-    @NotNull
-    @Size(max = 50)
-    private String contactnumber;
-
-
-
-
     @Override
     public boolean equals(Object obj) {
         return new EqualsBuilder().append(this, obj).isEquals();
@@ -208,6 +201,4 @@ public class UserEntity implements Serializable {
     public String toString() {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
-
-
 }
