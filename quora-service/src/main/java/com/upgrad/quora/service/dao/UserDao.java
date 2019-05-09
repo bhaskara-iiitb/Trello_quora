7a2b567289f08d21b9a2609ba936a05bd5a5e38b
Bhaskara Rayavarapu <bhaskara.dsd4@iiitb.net>
2019-05-10 01:48:03 +0530

    Updated revision of UserEndpoints (signup and signin) with exception handling and validated for test case PASS

--- quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -19,7 +19,6 @@ public class UserDao {
     public UserEntity createUser(UserEntity userEntity) {
         entityManager.persist(userEntity);
         return userEntity;
-
     }
 
     public UserEntity getUserByUsername(final String username) {
@@ -28,6 +27,19 @@ public class UserDao {
             return entityManager.createNamedQuery(
                     "userByUsername", UserEntity.class)
                     .setParameter("username", username)
+                    .getSingleResult();
+        }
+        catch(NoResultException nre) {
+            return null;
+        }
+    }
+
+    public UserEntity getUserByEmail(final String email) {
+
+        try {
+            return entityManager.createNamedQuery(
+                    "userByEmail", UserEntity.class)
+                    .setParameter("email", email)
                     .getSingleResult();
         }
         catch(NoResultException nre) {
